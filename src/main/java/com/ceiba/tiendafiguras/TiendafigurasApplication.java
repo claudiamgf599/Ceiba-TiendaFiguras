76623f3b0dad2d45dc0b85e27e30d73ffa49986e
Claudia María Gómez Flórez - Ceiba Software <claudia.gomez@ceiba.com.co>
2020-10-01 08:37:40 -0500

    Creacion del proyecto Tienda de figuras de coleccion

---
+++ src/main/java/com/ceiba/tiendafiguras/TiendafigurasApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.tiendafiguras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendafigurasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendafigurasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceiba/tiendafiguras/TiendafigurasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.tiendafiguras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendafigurasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
